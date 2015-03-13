47fd095ecb6e2ce3f25dd2342a2ed8cd9e9db202
Bram Cordie <bramcordie@gmail.com>
2015-03-13 10:46:02 +0100

    III-142: Add missing renamed file

---
+++ src/Entity/UsedLabelsMemoryEventStoreEntity.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\culturefeed_udb3\Entity\DomainMessageEntity.
+ */
+
+namespace Drupal\culturefeed_udb3\Entity;
+
+use Drupal\culturefeed_udb3\DomainMessageEntity;
+
+/**
+ * Defines the culturefeed udb3 used labels memory event store.
+ *
+ * @ContentEntityType(
+ *   id = "used_labels_memory_event_store",
+ *   label = @Translation("Culturefeed udb3 used labels memory event store"),
+ *   base_table = "culturefeed_udb3_used_labels_memory_event_store",
+ *   handlers = {
+ *     "storage_schema" = "Drupal\culturefeed_udb3\DomainMessageStorageSchema",
+ *   },
+ *   entity_keys = {
+ *     "id" = "dmid",
+ *   },
+ *   fieldable = FALSE,
+ * )
+ */
+class UsedLabelsMemoryEventStoreEntity extends DomainMessageEntity {
+}
