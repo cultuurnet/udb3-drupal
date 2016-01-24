b757572690f26256cc947e0d9061201eb6b8cea3
HnLn <hans@2dotstwice.be>
2016-01-24 21:26:08 +0100

    no message

--- src/CulturefeedUdb3ServiceProvider.php
+++ src/CulturefeedUdb3ServiceProvider.php
@@ -9,6 +9,7 @@ namespace Drupal\culturefeed_udb3;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
 use Drupal\Core\DependencyInjection\ServiceProviderInterface;
+use Drupal\Core\DependencyInjection\YamlFileLoader;
 
 class CulturefeedUdb3ServiceProvider extends ServiceProviderBase
 {
@@ -21,6 +22,11 @@ class CulturefeedUdb3ServiceProvider extends ServiceProviderBase
     public function register(ContainerBuilder $container)
     {
         $container->addCompilerPass(new RegisterEventBusSubscribersPass());
+
+        // Load separated service files.
+        $yaml_loader = new YamlFileLoader($container);
+        $path = __DIR__ . DIRECTORY_SEPARATOR . '..';
+        $yaml_loader->load($path . '/culturefeed_udb3.services.general.yml');
     }
 
     /**
