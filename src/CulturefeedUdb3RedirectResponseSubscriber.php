0a5174b0d04312fa80d9949bf18dfd20301189b0
HnLn <hans@2dotstwice.be>
2016-01-26 21:57:29 +0100

    III-705 Only unset the subscribers that are not needed.

--- src/CulturefeedUdb3RedirectResponseSubscriber.php
+++ src/CulturefeedUdb3RedirectResponseSubscriber.php
@@ -2,12 +2,13 @@
 
 /**
  * @file
- * Contains \Drupal\culturefeed_udb3\EventSubscriber\RedirectResponseSubscriber.
+ * Contains \Drupal\culturefeed_udb3\CulturefeedUdb3RedirectResponseSubscriber.
  */
 
 namespace Drupal\culturefeed_udb3;
 
 use Drupal\Core\EventSubscriber\RedirectResponseSubscriber;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * Allows manipulation of the response object when performing a redirect.
@@ -22,6 +23,21 @@ class CulturefeedUdb3RedirectResponseSubscriber extends RedirectResponseSubscrib
    */
   static function getSubscribedEvents() {
     // Unsubscribe the checkRedirectUrl and sanitizeDestination subscriptions.
-    return array();
+    $subscribed_events = parent::getSubscribedEvents();
+    $subscribed_events[KernelEvents::RESPONSE] = array_filter($subscribed_events[KernelEvents::RESPONSE], function($subscriber) {
+      return $subscriber[0] != 'checkRedirectUrl';
+    });
+    if (!count($subscribed_events[KernelEvents::RESPONSE])) {
+      unset($subscribed_events[KernelEvents::RESPONSE]);
+    }
+
+    $subscribed_events[KernelEvents::REQUEST] = array_filter($subscribed_events[KernelEvents::REQUEST], function($subscriber) {
+      return $subscriber[0] != 'sanitizeDestination';
+    });
+    if (!count($subscribed_events[KernelEvents::REQUEST])) {
+      unset($subscribed_events[KernelEvents::REQUEST]);
+    }
+
+    return $subscribed_events;
   }
 }
