a37d3ce9d2a8a958c49313270407b0131a214aa6
HnLn <hans@2dotstwice.be>
2016-01-30 19:47:44 +0100

    III-691 Fix worker command (autoload changed position).

--- culturefeed_udb3.worker.php
+++ culturefeed_udb3.worker.php
@@ -23,7 +23,7 @@ if (!$root) {
   exit();
 }
 
-$autoloader_path = $root . '/core/vendor/autoload.php';
+$autoloader_path = $root . '/autoload.php';
 if (!file_exists($autoloader_path)) {
   print "Drupal root is invalid\n";
   print "No vendor autoload found at $autoloader_path\n";
