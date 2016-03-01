69d423340bef883842dc37eb86d34155a3ae9b1f
Bert Ramakers <bertramakers@me.com>
2016-03-01 15:39:03 +0100

    III-680: Use interfaces for DI and simplified get() method a bit.

--- src/Factory/BulkLabelCommandHandlerFactory.php
+++ src/Factory/BulkLabelCommandHandlerFactory.php
@@ -2,11 +2,10 @@
 
 namespace Drupal\culturefeed_udb3\Factory;
 
+use Broadway\Repository\RepositoryInterface;
 use CultuurNet\UDB3\Offer\BulkLabelCommandHandler;
 use CultuurNet\UDB3\Offer\OfferType;
 use CultuurNet\UDB3\Search\ResultsGenerator;
-use CultuurNet\UDB3\UDB2\EventRepository;
-use CultuurNet\UDB3\UDB2\Place\PlaceRepository;
 
 /**
  * Class BulkLabelCommandHandlerFactory.
@@ -18,14 +17,14 @@ class BulkLabelCommandHandlerFactory {
   /**
    * The event repository.
    *
-   * @var \CultuurNet\UDB3\UDB2\EventRepository
+   * @var RepositoryInterface
    */
   protected $eventRepository;
 
   /**
    * The place repository.
    *
-   * @var \CultuurNet\UDB3\UDB2\Place\PlaceRepository
+   * @var RepositoryInterface
    */
   protected $placeRepository;
 
@@ -42,15 +41,15 @@ class BulkLabelCommandHandlerFactory {
    *
    * @param \CultuurNet\UDB3\Search\ResultsGenerator $search_results_generator
    *   The search results generator.
-   * @param \CultuurNet\UDB3\UDB2\EventRepository $event_repository
+   * @param RepositoryInterface $event_repository
    *   The event repository.
-   * @param \CultuurNet\UDB3\UDB2\Place\PlaceRepository $place_repository
+   * @param RepositoryInterface $place_repository
    *   The place repository.
    */
   public function __construct(
     ResultsGenerator $search_results_generator,
-    EventRepository $event_repository,
-    PlaceRepository $place_repository
+    RepositoryInterface $event_repository,
+    RepositoryInterface $place_repository
   ) {
     $this->searchResultsGenerator = $search_results_generator;
     $this->eventRepository = $event_repository;
@@ -64,12 +63,9 @@ class BulkLabelCommandHandlerFactory {
    *   The place json ld repository.
    */
   public function get() {
-
-    $bulk_command_handler = new BulkLabelCommandHandler($this->searchResultsGenerator);
-    $bulk_command_handler = $bulk_command_handler->withRepository(OfferType::EVENT(), $this->eventRepository);
-    $bulk_command_handler = $bulk_command_handler->withRepository(OfferType::PLACE(), $this->placeRepository);
-    return $bulk_command_handler;
-
+    return (new BulkLabelCommandHandler($this->searchResultsGenerator))
+      ->withRepository(OfferType::EVENT(), $this->eventRepository)
+      ->withRepository(OfferType::PLACE(), $this->placeRepository);
   }
 
 }
