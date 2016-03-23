4cd2ed527f248fd144e7f6e877058fadfcbad05c
HnLn <hans@2dotstwice.be>
2016-03-23 23:00:35 +0100

    Hotfix: ensure a base url is set in the iri generator.

--- src/IriGenerator.php
+++ src/IriGenerator.php
@@ -3,6 +3,7 @@
 namespace Drupal\culturefeed_udb3;
 
 use CultuurNet\UDB3\Iri\IriGeneratorInterface;
+use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Routing\UrlGeneratorInterface;
 
@@ -12,6 +13,13 @@ use Drupal\Core\Routing\UrlGeneratorInterface;
  * @package Drupal\culturefeed_udb3
  */
 class IriGenerator implements IriGeneratorInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $config;
 
   /**
    * Name of the route to show a single event.
@@ -39,14 +47,18 @@ class IriGenerator implements IriGeneratorInterface {
    *
    * @param UrlGeneratorInterface $url_generator
    *   The url generator.
+   * @param \Drupal\Core\Config\ConfigFactory $config
+   *   The config factory.
    * @param string $event_route_name
    *   The event route name.
    */
   public function __construct(
     UrlGeneratorInterface $url_generator,
+    ConfigFactory $config,
     $event_route_name = 'culturefeed_udb3.event'
   ) {
     $this->urlGenerator = $url_generator;
+    $this->config = $config->get('culturefeed_udb3.settings');
     $this->eventRouteName = $event_route_name;
   }
 
@@ -55,6 +67,7 @@ class IriGenerator implements IriGeneratorInterface {
    */
   public function iri($item) {
 
+    $base_url = $this->config->get('url');
     // @TODO
     // Implement cleaner approach once https://www.drupal.org/node/2616164 is
     // in.  For the same reason the language manager isn't injected, as there
@@ -68,6 +81,7 @@ class IriGenerator implements IriGeneratorInterface {
       array(
         'absolute' => TRUE,
         'language' => $language,
+        'base_url' => $base_url,
       )
     );
   }
