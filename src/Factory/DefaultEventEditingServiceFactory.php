277aebf7a657cd130f3c3240ca509076787cc31a
HnLn <hans@2dotstwice.be>
2016-03-29 20:58:28 +0200

    III-802 Move event editing service to factory.

---
+++ src/Factory/DefaultEventEditingServiceFactory.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Drupal\culturefeed_udb3\Factory;
+
+use Broadway\CommandHandling\CommandBusInterface;
+use Broadway\Repository\RepositoryInterface;
+use Broadway\UuidGenerator\UuidGeneratorInterface;
+use CultuurNet\UDB3\Event\DefaultEventEditingService;
+use CultuurNet\UDB3\Event\ReadModel\DocumentRepositoryInterface;
+use CultuurNet\UDB3\EventServiceInterface;
+use CultuurNet\UDB3\Offer\Commands\OfferCommandFactoryInterface;
+use CultuurNet\UDB3\PlaceService;
+use Drupal\Core\Config\ConfigFactory;
+
+class DefaultEventEditingServiceFactory {
+
+  /**
+   * The command bus.
+   *
+   * @var \Broadway\CommandHandling\CommandBusInterface
+   */
+  protected $commandBus;
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $config;
+
+  /**
+   * The event command factory.
+   *
+   * @var \CultuurNet\UDB3\Offer\Commands\OfferCommandFactoryInterface
+   */
+  protected $eventCommandFactory;
+
+  /**
+   * The event json ld repository.
+   *
+   * @var \CultuurNet\UDB3\Event\ReadModel\DocumentRepositoryInterface
+   */
+  protected $eventJsonLdRepository;
+
+  /**
+   * The event repository.
+   *
+   * @var \Broadway\Repository\RepositoryInterface
+   */
+  protected $eventRepository;
+
+  /**
+   * The event service.
+   *
+   * @var \CultuurNet\UDB3\EventServiceInterface
+   */
+  protected $eventService;
+
+  /**
+   * The place service.
+   *
+   * @var \CultuurNet\UDB3\PlaceService
+   */
+  protected $placeService;
+
+  /**
+   * The uuid generator.
+   *
+   * @var \Broadway\UuidGenerator\UuidGeneratorInterface
+   */
+  protected $uuidGenerator;
+
+  public function __construct(
+    EventServiceInterface $event_service,
+    CommandBusInterface $command_bus,
+    UuidGeneratorInterface $uuid_generator,
+    DocumentRepositoryInterface $event_json_ld_repository,
+    PlaceService $place_service,
+    OfferCommandFactoryInterface $event_command_factory,
+    RepositoryInterface $event_repository,
+    ConfigFactory $config
+  ) {
+    $this->eventService = $event_service;
+    $this->commandBus = $command_bus;
+    $this->uuidGenerator = $uuid_generator;
+    $this->eventJsonLdRepository = $event_json_ld_repository;
+    $this->placeService = $place_service;
+    $this->eventCommandFactory = $event_command_factory;
+    $this->eventRepository = $event_repository;
+    $this->config = $config->get('culturefeed_udb3.settings');
+  }
+
+  /**
+   * Get the default editing service.
+   *
+   * @return \CultuurNet\UDB3\Event\DefaultEventEditingService
+   *   The default editing service.
+   */
+  public function get() {
+
+    return new DefaultEventEditingService(
+      $this->eventService,
+      $this->commandBus,
+      $this->uuidGenerator,
+      $this->eventJsonLdRepository,
+      $this->placeService,
+      $this->eventCommandFactory,
+      $this->eventRepository
+    );
+
+  }
+
+}
